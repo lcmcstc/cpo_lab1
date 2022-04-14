7b3513b3be175a93596385afc15c693ecf9529c5
Lixiao <1240847431@qq.com>
2022-04-14 21:41:26 +0800

    blank lines and whitespace-v1

--- test_mutable.py
+++ test_mutable.py
@@ -2,27 +2,32 @@ import unittest
 from hypothesis import given
 import hypothesis.strategies as st
 from Hashmap_mutable import *
+
+
 class TestDict(unittest.TestCase):
     def test_size(self):
         print("start testing Dict.size")
         self.assertEqual(MyDictionary(3).size,3)
         self.assertEqual(MyDictionary(0).size,0)
         print("-----------over----------")
+
     def test_add(self):
         print("start testing Dict.add()")
         mydict = MyDictionary(5);
-        for i in range (10):
-            mydict.add(i,i)
+        for i in range(10):
+            mydict.add(i, i)
         mydict.print()
         print("-----------over----------")
+
     def test_set(self):
         print("Start testing Set")
         mydict = MyDictionary(5);
-        mydict.set(9,3)
+        mydict.set(9, 3)
         for i in range(3):
-            mydict.set(i,i)
+            mydict.set(i, i)
         mydict.print()
         print("-----------over----------")
+
     def test_remove(self):
         print("Start testing Remove")
         mydict = MyDictionary(5);
@@ -37,6 +42,7 @@ class TestDict(unittest.TestCase):
         print("Deleted value =",mydict.remove_by_value(3))
         mydict.print()
         print("-----------over----------")
+
     def test_access(self):
         print("start testing Dict.Access")
         mydict = MyDictionary(5);
@@ -50,14 +56,15 @@ class TestDict(unittest.TestCase):
         print("Dict.member()==contains_key(3)")
         print(mydict.contains_key(3))
         print("-----------over----------")
+
     def test_from_list(self):
         mydict = MyDictionary(5);
         for i in range(5):
             mydict.set(2 * i, i)
         print("to_list()")
-        ret=mydict.to_list()
+        ret = mydict.to_list()
         for i in range(5):
-            print(ret[i].key,ret[i].value)
+            print(ret[i].key, ret[i].value)
         print("-----------over----------")
 
 
